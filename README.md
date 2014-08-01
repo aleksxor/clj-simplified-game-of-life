6aa0705bf821cdd45f8c17ecb5f71429297717ff
Alexey Khoroshev <aleksxor@gmail.com>
2014-08-01 17:48:30 +0700

    added tests. fixed description

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Simplified game of life
 
 1-D game of life.
-On each turn only the cell with exactly 1 alive neighbour
-becomes/stays alive. Otherwise it dies.
+On each turn only the cell with exactly 1 alive neighbour (including
+itself) becomes/stays alive. Otherwise it dies.
 The initial universe generates randomly from the 0s and 1s.
 
 ## Usage
--- test/simple_life/core_test.clj
+++ test/simple_life/core_test.clj
@@ -3,5 +3,7 @@
             [simple-life.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "the next-gen func should generate the next generation of the universe"
+    (is (= (next-gen '(0 1 1 0)) '(1 0 0 1))))
+  (testing "the age func should iterate over 2 generations of the initial universe"
+    (is (= (age 2 '(0 1 1 0)) '(1 1 1 1)))))
